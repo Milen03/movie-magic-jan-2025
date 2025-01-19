9be82cd383744e3c10aa8ae1fbd8f1fdfb6cbd0d
Milen03 <milen.atanasovv03@gmail.com>
2025-01-19 16:01:37 +0200

    Add daunamig ling for movies

--- src/movies.js
+++ src/movies.js
@@ -1,17 +1,20 @@
 export default [
     {
+        id: 1,
         title: 'Jungle Cuise',
         genger: 'Adventure',
         description: 'Dreaming about saving countless lives and having another adventure, the feisty English feminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change the world. Along with her fashionable brother, MacGregor, Dr Houghton enlists the help of the arrogant, wisecracking riverboat skipper, Captain Frank Wolff, to guide them through the serpentine Amazon River in La Quila, his swift wooden boat. Now, as the intrepid trio ventures deeper and deeper into the heart of an impenetrable green maze, searching for something that cannot be found, a centuries-old curse and the ruthless aristocrat, Prince Joachim, threaten to put an end to their ambitious plans.',
         imageUrl: '/static/img/jungle-cruise.jpeg'
     },
     {
+        id: 2,
         title: 'Man of Steel',
         genre: 'Superhero',
         description: 'An alien child is evacuated from his dying world and sent to Earth to live among humans. His peace is threatened when other survivors of his home planet invade Earth.',
         imageUrl: 'https://m.media-amazon.com/images/M/MV5BMTk5ODk1NDkxMF5BMl5BanBnXkFtZTcwNTA5OTY0OQ@@._V1_.jpg'
     },
     {
+        id: 3,
         title: 'Под прикритие',
         genre: 'Crime',
         description: 'Trained young policeman goes undercover in the gang of the most dangerous mafia boss in Bulgaria.',
