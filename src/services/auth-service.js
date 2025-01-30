9d666ea45f9016e1f09420a2a35bfa8d63747591
Milen03 <milen.atanasovv03@gmail.com>
2025-01-30 20:25:27 +0200

    finich for day One

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -1,8 +1,26 @@
 import User from "../models/User.js";
-
+import bcrypt from 'bcrypt'
 export default {
-    register(userData){
-     return User.create(userData)
-        
+    register(userData) {
+        return User.create(userData)
+
+    },
+    async login(email, password) {
+
+        //Check user exists
+ const user =await User.findOne({ email })
+ if(!user){
+    throw new Error('Invalid email')
+ }
+
+        //Check password is correct
+const isValid = await bcrypt.compare(password,user.password)
+if(!isValid){
+    throw new Error('Invalid password!!')
+}
+        //Generate Token 
+
+        //return token 
+        return
     }
 }
