e0a79e252562607cd15fd36f70a1adb43c728411
Milen03 <milen.atanasovv03@gmail.com>
2025-01-30 18:56:33 +0200

    Send data to register end point

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -2,5 +2,17 @@ import { Router } from "express";
 
 const authController = Router()
 
+authController.get('/register',(req,res)=>{
+    res.render('auth/register')
+})
+
+authController.post('/register',async (req,res)=>{
+const newData = req.body
+
+console.log(newData);
+
+res.end()
+
+})
 
 export default authController
